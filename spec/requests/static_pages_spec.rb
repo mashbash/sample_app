6075734c4580f59b8dcc88af17c4ae7e48b6f3d6
Nigel Co <nigel.c1981@gmail.com>
2012-06-04 17:24:37 +0800

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  # used this for exercise 3.30, to avoid DRY. In tutorial, it is typed out as "Ruby on Rails Tutorial Sample App"
+  # if problems down the road, revert to Listing 4.4
 
   describe "Home page" do
 
@@ -11,9 +13,15 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title',
+                        :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
