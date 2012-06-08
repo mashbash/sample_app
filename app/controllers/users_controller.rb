6471f07561ac4206b3333480404823ae4d6def9c
Nigel Co <nigel.c1981@gmail.com>
2012-06-09 05:15:33 +0800

    Finished up to 9.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,6 @@ class UsersController < ApplicationController
   end
 
   def new
-  end
-
-  def new
     @user = User.new
   end
 
@@ -21,5 +18,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+   if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else 
+    render 'edit'
+    end
+  end
 end
