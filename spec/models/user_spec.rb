e634735e398096b65f6e5a91afe1788be1e4f0c7
Nigel Co <nigel.c1981@gmail.com>
2012-06-10 07:35:38 +0800

    FInish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,10 +30,26 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
   # calls valid ? subject on test
+  it { should_not be_admin }
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+         User.new(admin: "1") 
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,56 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+
+  let(:user) { FactoryGirl.create(:user) }
+
+    before(:all) { 30.times { FactoryGirl.create(:user) } }
+    after(:all)  { User.delete_all }
+
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    describe "pagination" do
+
+        it { should have_selector('div.pagination') }
+
+        it "should list each user" do
+          User.paginate(page: 1).each do |user|
+            #make this test more stringent, test for anchor tag by adding >a
+            page.should have_selector('li>a', text: user.name)
+          end
+        end
+      end
+    end
+
+    describe "delete links" do
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        before(:all) { 5.times { FactoryGirl.create(:user) } }
+        after(:all)  { User.delete_all }
+        # 9.4.2 - 2 lines of code above added to fix test suite errors, not from tutorial but web
+        let(:admin) { FactoryGirl.create(:admin) }
+        
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -66,7 +116,10 @@ describe "User pages" do
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it { should have_selector('h1',    text: "Update your profile") }
@@ -98,4 +151,5 @@ describe "User pages" do
       specify { user.reload.email.should == new_email }
     end
   end
+
 end
